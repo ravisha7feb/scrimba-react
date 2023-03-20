180092b755365d037b6ef433831f671f5ddea43f
ravisha7feb <64865136+ravisha7feb@users.noreply.github.com>
2023-03-21 00:04:23 +0530

    add tenzies game and modify necessary colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import "../style.css";
 import BusinessCard from "./components/Business_card/Business_Card";
 import TravelJournal from "./components/Travel_Journal/Travel_Journal";
+import Tenzies from "./components/Tenzies/tenzies"
 
 function App(){
     return (
         <div className="app">
             {/* <BusinessCard /> */}
             
-            <TravelJournal />
+            {/* <TravelJournal /> */}
+
+            <Tenzies />
+
         </div>
     )
 }
